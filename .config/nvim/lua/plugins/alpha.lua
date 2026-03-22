3b9cd104a3dd1cb812080a08a72a23718313a171
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2026-03-22 11:14:14 +0100

    upd alpha.lua

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,41 +1,84 @@
+--vim.api.nvim_create_autocmd("VimEnter", {
+    --once = true,
+    --callback = function()
+        --local alpha = require("alpha")
+        --local dashboard = require("alpha.themes.dashboard")
+--
+        ---- Set header
+        --dashboard.section.header.val = {
+            --"                                                     ",
+            --"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            --"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            --"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            --"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            --"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            --"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            --"                                                     ",
+        --}
+--
+        ---- Set menu
+        --dashboard.section.buttons.val = {
+            --dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+            ---- dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+            --dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Oil<CR>"),
+            ---- dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
+            --dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+            --dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+            --dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+            --dashboard.button("b", "󱆃  > ~/.bashrc", "<cmd>e ~/.bashrc<CR>"),
+            --dashboard.button("f", "󱆃  > ~/.config/fish/config.fish", "<cmd>e ~/.config/fish/config.fish<CR>"),
+            --dashboard.button("k", "󰄛  > ~/.config/kitty/kitty.conf", "<cmd>e ~/.config/kitty/kitty.conf<CR>"),
+            --dashboard.button("h", "󰀫  > Alpha-nvim", "<cmd>e ~/.config/nvim/lua/plugins/alpha-nvim.lua<CR>"),
+            --dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+        --}
+--
+        ---- Send config to alpha
+        --alpha.setup(dashboard.opts)
+--
+        ---- Disable folding on alpha buffer
+        --vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    --end,
+--})
+
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
+-- Set header
+dashboard.section.header.val = {
+  "                                                     ",
+  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+  "                                                     ",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+  dashboard.button("e", " > New File", "<cmd>ene<CR>"),
+  dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Oil<CR>"),
+  dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+  dashboard.button("SPC fs", "󱎸 > Find Word", "<cmd>Telescope live_grep<CR>"),
+  dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+  dashboard.button("b", "󱆃  > ~/.bashrc", "<cmd>e ~/.bashrc<CR>"),
+  dashboard.button("f", "󱆃  > ~/.config/fish/config.fish", "<cmd>e ~/.config/fish/config.fish<CR>"),
+  dashboard.button("k", "󰄛  > ~/.config/kitty/kitty.conf", "<cmd>e ~/.config/kitty/kitty.conf<CR>"),
+  dashboard.button("h", "󰀫  > Alpha-nvim", "<cmd>e ~/.config/nvim/lua/plugins/alpha.lua<CR>"),
+  dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+}
+
+-- Setup alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+
+-- Show alpha on VimEnter
 vim.api.nvim_create_autocmd("VimEnter", {
-    once = true,
-    callback = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
-
-        -- Set header
-        dashboard.section.header.val = {
-            "                                                     ",
-            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-            "                                                     ",
-        }
-
-        -- Set menu
-        dashboard.section.buttons.val = {
-            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-            -- dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-            dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Oil<CR>"),
-            -- dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
-            dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-            dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-            dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-            dashboard.button("b", "󱆃  > ~/.bashrc", "<cmd>e ~/.bashrc<CR>"),
-            dashboard.button("f", "󱆃  > ~/.config/fish/config.fish", "<cmd>e ~/.config/fish/config.fish<CR>"),
-            dashboard.button("k", "󰄛  > ~/.config/kitty/kitty.conf", "<cmd>e ~/.config/kitty/kitty.conf<CR>"),
-            dashboard.button("h", "󰀫  > Alpha-nvim", "<cmd>e ~/.config/nvim/lua/plugins/alpha-nvim.lua<CR>"),
-            dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
-        }
-
-        -- Send config to alpha
-        alpha.setup(dashboard.opts)
-
-        -- Disable folding on alpha buffer
-        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-    end,
+  once = true,
+  callback = function()
+    alpha.start(true)
+  end,
 })
