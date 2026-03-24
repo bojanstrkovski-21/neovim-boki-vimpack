48e16afc9094cdd496c9cf7521254150ad9aaf83
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2026-03-24 20:23:11 +0100

    telescope upd to make libfzf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,54 @@
+--local telescope = require("telescope")
+--local actions = require("telescope.actions")
+--local builtin = require("telescope.builtin")
+--
+--require("mini.icons").setup()
+--
+--telescope.load_extension("fzf")
+--telescope.load_extension("themes")
+--
+--telescope.setup({
+    --defaults = {
+        --path_display = { "smart" },
+        --mappings = {
+            --i = {
+                --["<C-k>"] = actions.move_selection_previous,
+                --["<C-j>"] = actions.move_selection_next,
+            --},
+        --},
+    --},
+    --extensions = {
+        --themes = {
+            --enable_previewer = true,
+            --enable_live_preview = true,
+            --persist = {
+                --enabled = true,
+                --path = vim.fn.stdpath("config") .. "/lua/colors-2025.lua",
+            --},
+        --},
+    --},
+--})
+--
+--vim.keymap.set("n", "<leader>pr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+--vim.keymap.set("n", "<leader>pWs", function()
+    --local word = vim.fn.expand("<cWORD>")
+    --builtin.grep_string({ search = word })
+--end, { desc = "Find Connected Words under cursor" })
+--vim.keymap.set("n", "<leader>vb", "<cmd>Telescope themes<CR>", { noremap = false, silent = true, desc = "Theme Switcher" })
+
+local fzf_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/telescope-fzf-native.nvim"
+local lib = fzf_path .. "/build/libfzf.so"
+
+if vim.fn.filereadable(lib) == 0 then
+    vim.fn.system("make -C " .. fzf_path)
+end
+
 local telescope = require("telescope")
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
-
 require("mini.icons").setup()
-
 telescope.load_extension("fzf")
 telescope.load_extension("themes")
-
 telescope.setup({
     defaults = {
         path_display = { "smart" },
@@ -28,7 +70,6 @@ telescope.setup({
         },
     },
 })
-
 vim.keymap.set("n", "<leader>pr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
 vim.keymap.set("n", "<leader>pWs", function()
     local word = vim.fn.expand("<cWORD>")
